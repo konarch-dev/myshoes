181d71aa205189400c2f59eb56611e0c25ab1f16
konarch prasad <98590133+konarch-dev@users.noreply.github.com>
2022-11-30 05:00:40 +0530

    update
    update changes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,20 @@ Route::get('welcome/', function () {
 });
 
 
+Route::get('/currencyapi', function () {
+ $url_name = "https://v6.exchangerate-api.com/v6/8aa0ca713a87af935d0a12ce/latest/USD";
+
+  $ch_session = curl_init();
+
+curl_setopt($ch_session, CURLOPT_RETURNTRANSFER, 1);
+
+  curl_setopt($ch_session, CURLOPT_URL, $url);
+
+  $result_url = curl_exec($ch_session);
+
+  return $result_url;
+});
+
 
 Route::get('/', function () {
     return view('index');
